d47b1ac32b0ce14973695fdb672d0dc233206363
Dipak Giri <dipakgiri1200@gmail.com>
2024-09-21 17:46:50 +0545

    fix api/cron route , not firing in production

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -1,5 +1,6 @@
 import type { NextRequest } from "next/server";
 
+export const revalidate = 0;
 export function GET(request: NextRequest) {
   // const authHeader = request.headers.get("authorization");
   // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
